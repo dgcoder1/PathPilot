9ee4f6172e25731da05284eed66abb11406b4f5a
dgcoder1 <diorfely7464@gmail.com>
2026-07-28 14:41:23 -0400

    Initial Commit

---
+++ PathPilot/PathPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathPilotApp.swift
+//  PathPilot
+//
+//  Created by Diorfely Gomez on 7/28/26.
+//
+
+import SwiftUI
+
+@main
+struct PathPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
